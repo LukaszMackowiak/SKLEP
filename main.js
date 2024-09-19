1bdc3b9568d101ba6c502751a9fe94b2d3760848
LukaszMackowiak <102365309+LukaszMackowiak@users.noreply.github.com>
2024-09-19 12:16:12 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const wzor_1 = document.getElementById("wzor1");
+const wzor_2 = document.getElementById("wzor2");
+const wzor_3 = document.getElementById("wzor3");
+const wzor_4 = document.getElementById("wzor4");
+const wzor_5 = document.getElementById("wzor5");
+const wzor_6 = document.getElementById("wzor6");
+
+const ilosc_1 = wzor_1.value;
+const ilosc_2 = wzor_2.value;
+const ilosc_3 = wzor_3.value;
+const ilosc_4 = wzor_4.value;
+const ilosc_5 = wzor_5.value;
+const ilosc_6 = wzor_6.value;
